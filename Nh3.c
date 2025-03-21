51393bc2e5b3de67db36e534a258ac5f05efc47d
zailu-tech <luizaabreudossantos97@gmail.com>
2025-03-21 17:50:51 -0300

    Atualizando Nh3.c

--- Nh3.c
+++ Nh3.c
@@ -1,13 +1,15 @@
-#include <stdio.h>
-#include <string.h>
-
+#include <stdio.h> // Incluindo a biblioteca padrão de entrada e saída
+#include <string.h> // Incluindo a biblioteca para manipulação de strings
+#include <stdlib.h> // Incluindo a biblioteca padrão
+#include <ctype.h> // Incluindo a biblioteca para manipulação de caracteres
 
 
 #define MAX_TENTATIVAS 5 // Definindo o número máximo de tentativas
 
 int main(){
 
- char palavra[]= "Lua";
+ char *palavras[]= {"Lua", "Sol", "Planeta", "Terra", "gato", "Sapato", "Estrela", "hora"}; // Palavra a ser adivinhada
+ char *palavra = palavras[rand() % 8]; // Palavra a ser adivinhada
  char tentativasFeitas[strlen(palavra) +1]; // Vetor para armazenar as tentativas feitas
 int tentativas = 0; // Número de tentativas feitas
 int acertos = 0; // Número de acertos
@@ -30,10 +32,11 @@ while(tentativas < MAX_TENTATIVAS){ // Enquanto o número de tentativas for meno
 
     char tentativa;      // Lendo a tentativa 
     scanf(" %c", &tentativa); 
+    tentativa= tolower(tentativa); // Convertendo a tentativa para minúscula
 
     int encontrado =0; // Verificando se a letra foi encontrada
     for(int i = 0; i < tamanhoPalavra; i++){ // Verificando se a letra foi encontrada
-        if(palavra[i] == tentativa && tentativasFeitas[i] == '-'){ 
+        if(tolower(palavra[i]) == tentativa && tentativasFeitas[i] == '-'){ 
             tentativasFeitas[i] = tentativa; 
             acertos++;
             encontrado = 1; 
@@ -43,7 +46,7 @@ while(tentativas < MAX_TENTATIVAS){ // Enquanto o número de tentativas for meno
         tentativas++;
     }
     if(acertos == tamanhoPalavra){
-        printf("\nParabéns! Você acertou a palavra!\n");
+        printf("\nParabéns! Você acertou! A palavra secreta era: %s\n", palavra);
         break;
     }
 }
